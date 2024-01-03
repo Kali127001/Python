736b29586223b9f2397f7883033666e75deb56e9
kali127001 <kalki8358@gmail.com>
2024-01-03 22:19:44 +0530

    add python files

---
+++ 02-Day/py4e/conditionals.py
@@ -0,0 +1,23 @@
+x = 5
+
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+
+print('Finish')
+
+y = 5
+
+if y == 5:
+    print('Equal to 5')
+if y > 4:
+    print('Greater than 4')
+if y >= 5:
+    print('Greater than or Equals 5')
+if y < 6:
+    print('Less than 6')
+if y <= 5:
+    print('Less than or equal to 5')
+if y != 6:
+    print('Not equals to 6')
---
+++ 02-Day/py4e/input.py
@@ -0,0 +1,9 @@
+name = input('Who are you?\n')
+print('Welcome', name)
+
+# Elevator Floor Conversion App
+
+inp = input('Europe floor?\n')
+usf = int(inp) + 1
+
+print("US floor", usf)
